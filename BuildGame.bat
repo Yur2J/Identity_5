de0af857310023c665238ed19b3f017dd5024efd
JeongYuri <2024184029@tukorea.ac.kr>
2026-06-19 17:41:51 +0900

    감시자 Attack 기능과 충돌 체크 기능 추가
    생존자 에셋 구해서 리타겟 후 로직 진행해야 함

--- BuildGame.bat
+++ BuildGame.bat
@@ -1,2 +1,2 @@
 call "C:/Program Files/Epic Games/UE_5.7/Engine/Build/BatchFiles/Build.bat" ^
--projectfiles -project="%~dp0/NetworkStudy.uproject" -game -rocket -progress
+-projectfiles -project="%~dp0/Identity_5.uproject" -game -rocket -progress
